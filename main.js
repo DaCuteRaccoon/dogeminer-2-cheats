97acdf663c631ee2c553cae39ad842fcd761db40
Jack5079 <29169102+Jack5079@users.noreply.github.com>
2019-08-05 23:56:22 -0400

    first stuff

---
+++ main.js
@@ -0,0 +1,18 @@
+let int = document.getElementById('int')
+let dps = document.getElementById('dps')
+let autoclick = document.getElementById('click')
+
+let dpscheat = ''
+let clickcheat = ''
+
+function update () {
+  if (dps.checked) dpscheat = `setInterval(dogeminer.bonus.addSpecialBonus, int);`
+  if (autoclick.checked) clickcheat = `setInterval(dogeminer.rock.mineRock, int);`
+  document.getElementsByTagName('a')[0].href = `javascript: let int=${int.value ? int.value : 1};${dpscheat}${clickcheat}void 0;`
+}
+
+int.addEventListener('input', update)
+dps.addEventListener('click', update)
+autoclick.addEventListener('click', update)
+
+update()
