9404b69788e41c881d393ca179ae881750445411
Jack5079 <29169102+Jack5079@users.noreply.github.com>
2019-08-23 18:58:58 -0400

    better

--- main.js
+++ main.js
@@ -6,23 +6,10 @@ const dps = document.getElementById('dps')
 const loot = document.getElementById('loot')
 const autoclick = document.getElementById('click')
 
-let dpscheat = '' // Init the dps cheat
-let clickcheat = '' // Init the autoclick cheat
-let lootcheat = '' // Init the loot cheat
-
-document.querySelector('a').onclick = (e) => { e.preventDefault(); window.alert('Use it on the Dogeminer 2 website, not here.') } // Alert user if they try to use the cheat on the cheat website, instead of the real game.
+document.querySelector('a').onclick = e => { e.preventDefault(); window.alert('Use it on the Dogeminer 2 website, not here.') } // Alert user if they try to use the cheat on the cheat website, instead of the real game.
 
 function update () {
-  if (dps.checked) {
-    dpscheat = 'setInterval(dogeminer.bonus.addSpecialBonus, int);'
-  } else dpscheat = ''
-  if (autoclick.checked) {
-    clickcheat = 'setInterval(dogeminer.rock.mineRock, int);'
-  } else clickcheat = ''
-  if (loot.checked) {
-    lootcheat = 'setInterval(dogeminer.loot.devLoot, int);'
-  } else lootcheat = ''
-  document.getElementsByTagName('a')[0].href = `javascript: let int=${int.value ? int.value : 1};${dpscheat}${clickcheat}${lootcheat}void 0;`
+  document.querySelector('a').href = `javascript: let int=${int.value ? int.value : 1};${dps.checked ? 'setInterval(dogeminer.bonus.addSpecialBonus, int);' : ''}${autoclick.checked ? 'setInterval(dogeminer.rock.mineRock, int);' : ''}${loot.checked ? 'setInterval(dogeminer.loot.devLoot, int);' : ''}void 0;`
 }
 
 int.addEventListener('input', update)
