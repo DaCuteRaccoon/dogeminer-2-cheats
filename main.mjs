04c11651705307dadd354a4d320037dba6149552
Jack <29169102+Jack5079@users.noreply.github.com>
2019-10-28 19:57:24 -0400

    Redesign the website, and add a new cheat

--- main.mjs
+++ main.mjs
@@ -5,11 +5,18 @@ const int = document.getElementById('int')
 const dps = document.getElementById('dps')
 const loot = document.getElementById('loot')
 const autoclick = document.getElementById('click')
+const strength = document.getElementById('strength')
 
 document.querySelector('a').onclick = e => { e.preventDefault(); window.alert('Use it on the Dogeminer 2 website, not here.') } // Alert user if they try to use the cheat on the cheat website, instead of the real game.
 
 function update () {
-  document.querySelector('a').href = `javascript: let int=${int.value ? int.value : 1};${dps.checked ? 'setInterval(dogeminer.bonus.addSpecialBonus, int);' : ''}${autoclick.checked ? 'setInterval(dogeminer.rock.mineRock, int);' : ''}${loot.checked ? 'setInterval(dogeminer.loot.devLoot, int);' : ''}void 0;`
+  document.querySelector('a').href = `javascript:
+  let int=${int.value || 1};
+  ${dps.checked ? 'setInterval(dogeminer.bonus.addSpecialBonus, int)' : ''};
+  ${autoclick.checked ? 'setInterval(dogeminer.rock.mineRock, int)' : ''};
+  ${loot.checked ? 'setInterval(dogeminer.loot.devLoot, int)' : ''};
+  ${strength.checked ? 'setInterval(()=>{dogeminer.game.extrastrength++}, int)' : ''};
+  void 0;`
 }
 
 int.addEventListener('input', update)
--- splashes.mjs
+++ splashes.mjs
@@ -3,7 +3,8 @@ const splashes = [ // The splashes at the top of the page.
   'Not approved by rkn!',
   'Hacks by Jack5079!',
   'Why does this need splashes?',
-  'Not <cite>Minecraft</cite>!'
+  'Not <cite>Minecraft</cite>!',
+  'I was bored...'
 ]
 
 export default () => splashes[Math.floor(Math.random() * splashes.length)] // Export the default function, which randomly chooses a splash.
